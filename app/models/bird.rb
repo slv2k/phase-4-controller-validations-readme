6b0c9f35936ff9666d59753269abad747ebd36b2
samuel <samuelmmanor@gmail.com>
2022-03-14 22:38:01 -0400

    commit tree 5d39c4f6482a98ce559eff79a652b2ccfac02643

--- app/models/bird.rb
+++ app/models/bird.rb
@@ -1,2 +1,14 @@
 class Bird < ApplicationRecord
+    validates :name, presence: true, uniqueness: true
+
+    def create
+        bird = Bird.create!(bird_params)
+        render json: bird, status: :created
+    end
+
+    def update
+        bird = find_bird
+        bird.update!(bird_params)
+        render json: bird
+    end
 end
